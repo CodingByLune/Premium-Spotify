64347f1fc83ff83e6b64d6d96fe857b2884b9f43
Lune <94059698+CodingByLune@users.noreply.github.com>
2021-11-10 15:47:59 +0100

    Update Spotify Launcher.bat

--- Spotify Launcher.bat
+++ Spotify Launcher.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/CodingByLune/Lune/main/install.ps1?token=AWNTZMT33NNUDVM754VNTETBRPLR4' | Invoke-Expression}"
+powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/CodingByLune/Premium-Spotify/main/install.ps1' | Invoke-Expression}"
 pause
 exit
