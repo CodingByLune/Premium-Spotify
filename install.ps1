664e9a75cc3b144cbbb3a154e491dfd826fe39fb
Lune <94059698+CodingByLune@users.noreply.github.com>
2022-04-26 10:22:33 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -242,13 +242,14 @@ pause
 
 cls
 
-start https://discord.gg/XT83VNqG6A
+start https://discord.gg/mKGEeEV684
 start https://github.com/CodingByLune
 
 write-host @'
 ***************** 
 IF YOU NEED HELP CONTACT ME ON DISCORD : Lune#6666
 OR TIKTOK : Mrt_Lune
+OR DM ME ON https://discord.gg/mKGEeEV684
 AND IF YOU DON'T NEED HELP JUST LEAVE A COMMENT :) 
 FOR SUPPORT ME.
 THANKS YOU FOR DOWNLOAD SPOTIFY BY LUNE
